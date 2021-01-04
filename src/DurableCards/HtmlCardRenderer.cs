a313969268176d2869d8b309e18d090830c5d6ad
Andy Booth <andybooth@users.noreply.github.com>
2021-01-04 22:03:00 +0000

    Move extensions to own file

--- src/DurableCards/HtmlCardRenderer.cs
+++ src/DurableCards/HtmlCardRenderer.cs
@@ -243,26 +243,3 @@ namespace DurableCards
         }
     }
 }
-
-public static class HtmlTagExtensions
-{
-    public static HtmlTag Attr(this HtmlTag htmlTag, string name, bool enabled)
-    {
-        if (enabled)
-        {
-            htmlTag.Attr(name, name);
-        }
-
-        return htmlTag;
-    }
-
-    public static HtmlTag AddOptionalClass(this HtmlTag htmlTag, string className)
-    {
-        if (string.IsNullOrEmpty(className))
-        {
-            return htmlTag;
-        }
-
-        return htmlTag.AddClass(className);
-    }
-}
---
+++ src/DurableCards/HtmlTagExtensions.cs
@@ -0,0 +1,27 @@
+﻿using AdaptiveCards.Rendering.Html;
+
+namespace DurableCards
+{
+    public static class HtmlTagExtensions
+    {
+        public static HtmlTag Attr(this HtmlTag htmlTag, string name, bool enabled)
+        {
+            if (enabled)
+            {
+                htmlTag.Attr(name, name);
+            }
+
+            return htmlTag;
+        }
+
+        public static HtmlTag AddOptionalClass(this HtmlTag htmlTag, string className)
+        {
+            if (string.IsNullOrEmpty(className))
+            {
+                return htmlTag;
+            }
+
+            return htmlTag.AddClass(className);
+        }
+    }
+}
